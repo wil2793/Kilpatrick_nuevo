bff87d379ec964805d02859ebadea89c3d16e5eb
William <wilitupcr27@gmail.com>
2018-05-01 12:16:08 -0500

    Agregue una pagina web

---
+++ prueba.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>documento de prueba</title>
+</head>
+<body>
+	<h1>Esta es un prueba xD</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat esse necessitatibus quasi pariatur rem minima quae recusandae officiis, aliquam expedita sequi, sunt. Quibusdam recusandae quisquam veniam, blanditiis, nemo odit dolore.</p>
+</body>
+</html>
