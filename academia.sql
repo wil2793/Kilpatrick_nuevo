af808102e070bd9959606f73ff3701b2d5651ecc
erikescoffie <38329643+erikescoffie@users.noreply.github.com>
2018-05-01 23:35:56 -0500

    Es la nueva BD

--- academia.sql
+++ academia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-04-2018 a las 20:20:23
+-- Tiempo de generación: 02-05-2018 a las 06:34:34
 -- Versión del servidor: 10.1.31-MariaDB
 -- Versión de PHP: 7.2.3
 
@@ -35,6 +35,14 @@ CREATE TABLE `administrador` (
   `rcontrasenia` varchar(255) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `administrador`
+--
+
+INSERT INTO `administrador` (`correo`, `nombre`, `contrasenia`, `rcontrasenia`) VALUES
+('liz@gmail.com', 'Lizbeth', '1234', '1234'),
+('lupe@gmail.com', 'Lupe', '1234', '1234');
+
 -- --------------------------------------------------------
 
 --
@@ -49,9 +57,15 @@ CREATE TABLE `alumno` (
   `rcontrasenia` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
   `genero` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `edad` int(3) NOT NULL,
-  `grado` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
-  `nombregrupo` varchar(255) COLLATE utf8_spanish_ci NOT NULL
+  `grado` varchar(20) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`matricula`, `nombrealumno`, `correo`, `contrasenia`, `rcontrasenia`, `genero`, `edad`, `grado`) VALUES
+('E427367', 'Victo', 'vic@gmail.comete', '1234', '1234', 'Hombre', 24, '3');
 
 -- --------------------------------------------------------
 
@@ -83,7 +97,10 @@ CREATE TABLE `grupos` (
 --
 
 INSERT INTO `grupos` (`nombregrupo`, `nombremaestro`, `nombremateria`) VALUES
-('3SB', 'Yamil', 'Fisica II');
+('Aero', 'Yamil', 'Telecomunicaciones'),
+('H5', 'Yamil', 'Telecomunicaciones'),
+('H6', 'Yamil', 'Sistemas Operativos'),
+('H7', 'Panchito', 'Inteligencia Artificial');
 
 -- --------------------------------------------------------
 
@@ -103,8 +120,10 @@ CREATE TABLE `maestro` (
 --
 
 INSERT INTO `maestro` (`correo`, `nombremaestro`, `contrasenia`, `rcontrasenia`) VALUES
+('berme@gmail.com', 'Bermejos', '1234', '1234'),
+('eu@gmail.com', 'Eunice', '1234', '1234'),
 ('liz@gmail.com', 'Lizbeth', '1234', '1234'),
-('pati@gmail.com', 'Patricia', '1234', '1234'),
+('panchito@gmail.com', 'Panchito', '1234', '1234'),
 ('yamil@gmail.com', 'Yamil', '1234', '1234');
 
 -- --------------------------------------------------------
@@ -122,9 +141,12 @@ CREATE TABLE `materias` (
 --
 
 INSERT INTO `materias` (`nombremateria`) VALUES
-('Fisica II'),
+('BBB'),
+('In IV'),
+('Inteligencia Artificial'),
 ('quimica'),
-('Sistemas Operativos');
+('Sistemas Operativos'),
+('Telecomunicaciones');
 
 --
 -- Índices para tablas volcadas
@@ -134,15 +156,15 @@ INSERT INTO `materias` (`nombremateria`) VALUES
 -- Indices de la tabla `administrador`
 --
 ALTER TABLE `administrador`
-  ADD PRIMARY KEY (`correo`);
+  ADD PRIMARY KEY (`correo`),
+  ADD KEY `contrasenia` (`contrasenia`);
 
 --
 -- Indices de la tabla `alumno`
 --
 ALTER TABLE `alumno`
   ADD PRIMARY KEY (`matricula`),
-  ADD UNIQUE KEY `matricula` (`nombrealumno`),
-  ADD UNIQUE KEY `nombregrupo` (`nombregrupo`);
+  ADD KEY `matricula` (`nombrealumno`) USING BTREE;
 
 --
 -- Indices de la tabla `calificaciones`
@@ -157,8 +179,8 @@ ALTER TABLE `calificaciones`
 --
 ALTER TABLE `grupos`
   ADD PRIMARY KEY (`nombregrupo`),
-  ADD UNIQUE KEY `nombremaestro` (`nombremaestro`),
-  ADD UNIQUE KEY `nombremateria` (`nombremateria`);
+  ADD KEY `nombremaestro` (`nombremaestro`) USING BTREE,
+  ADD KEY `nombremateria` (`nombremateria`) USING BTREE;
 
 --
 -- Indices de la tabla `maestro`
@@ -178,25 +200,19 @@ ALTER TABLE `materias`
 --
 
 --
--- Filtros para la tabla `alumno`
---
-ALTER TABLE `alumno`
-  ADD CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`nombregrupo`) REFERENCES `grupos` (`nombregrupo`) ON UPDATE CASCADE;
-
---
 -- Filtros para la tabla `calificaciones`
 --
 ALTER TABLE `calificaciones`
-  ADD CONSTRAINT `calificaciones_ibfk_1` FOREIGN KEY (`matricula`) REFERENCES `alumno` (`matricula`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `calificaciones_ibfk_2` FOREIGN KEY (`nombrealumno`) REFERENCES `alumno` (`nombrealumno`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `calificaciones_ibfk_3` FOREIGN KEY (`nombremateria`) REFERENCES `materias` (`nombremateria`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `calificaciones_ibfk_1` FOREIGN KEY (`matricula`) REFERENCES `alumno` (`matricula`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `calificaciones_ibfk_2` FOREIGN KEY (`nombrealumno`) REFERENCES `alumno` (`nombrealumno`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `calificaciones_ibfk_3` FOREIGN KEY (`nombremateria`) REFERENCES `materias` (`nombremateria`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `grupos`
 --
 ALTER TABLE `grupos`
-  ADD CONSTRAINT `grupos_ibfk_1` FOREIGN KEY (`nombremateria`) REFERENCES `materias` (`nombremateria`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `grupos_ibfk_2` FOREIGN KEY (`nombremaestro`) REFERENCES `maestro` (`nombremaestro`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `grupos_ibfk_1` FOREIGN KEY (`nombremateria`) REFERENCES `materias` (`nombremateria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `grupos_ibfk_2` FOREIGN KEY (`nombremaestro`) REFERENCES `maestro` (`nombremaestro`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
